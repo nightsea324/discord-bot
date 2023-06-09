5e7365249e8238f25212726ce5c7a68f9f87aee0
NightSea <sweetmilkshake0324@gmail.com>
2023-06-09 11:53:10 +0800

    feat: Discord bot 通知完成

--- app.py
+++ app.py
@@ -1,87 +1,125 @@
-import discord
-import requests
-from aiohttp import web
 import os
+# discord
+import discord
+from discord.ext import commands
+from discord.ext.tasks import loop
 # twitch
-from twitchAPI.twitch import Twitch
-from twitchAPI.helper import first
-from twitchAPI.eventsub import EventSub
-from twitchAPI.oauth import UserAuthenticator
-from twitchAPI.types import AuthScope
-import asyncio
+import requests
 
 from dotenv import load_dotenv
 load_dotenv()
 
 # twitch
-TARGET_USERNAME = "nightsea324"
-EVENTSUB_URL = os.getenv("TWITCH_CALLBACK_URL")
-APP_ID = os.getenv("TWITCH_CLIENT_ID")
-APP_SECRET = os.getenv("TWITCH_CLIENT_SECRET")
-TARGET_SCOPES = [AuthScope.MODERATOR_READ_FOLLOWERS]
+TARGET_USERNAME = os.getenv("TWITCH_USERNAME").split(",")
+TWITCH_CLIENT_ID = os.getenv("TWITCH_CLIENT_ID")
+TWITCH_CLIENT_SECRET = os.getenv("TWITCH_CLIENT_SECRET")
+ACCESS_TOKEN = os.getenv("TWITCH_ACCESS_TOKEN")
+# discord
+TOKEN = os.getenv("DISCORD_TOKEN")
+DISCORD_CHANNEL_ID = int(os.getenv("DISCORD_CHANNEL_ID"))
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 
 # twitch
-async def on_follow(data: dict):
-    # our event happend, lets do things with the data we got!
-    print(data)
+def get_app_access_token():
+    params = {
+        "client_id": TWITCH_CLIENT_ID,
+        "client_secret": TWITCH_CLIENT_SECRET,
+        "grant_type": "client_credentials"
+    }
+
+    response = requests.post("https://id.twitch.tv/oauth2/token", params=params)
+    access_token = response.json()["access_token"]
+    return access_token
 
 
-async def eventsub_example():
-    # create the api instance and get the ID of the target user
-    twitch = await Twitch(APP_ID, APP_SECRET)
-    user = await first(twitch.get_users(logins=TARGET_USERNAME))
-
-    # the user has to authenticate once using the bot with our intended scope.
-    # since we do not need the resulting token after this authentication, we just discard the result we get from authenticate()
-    # Please read up the UserAuthenticator documentation to get a full view of how this process works
-    auth = UserAuthenticator(twitch, TARGET_SCOPES, force_verify=False)
-    await auth.authenticate()
-
-    # basic setup, will run on port 8080 and a reverse proxy takes care of the https and certificate
-    event_sub = EventSub(EVENTSUB_URL, APP_ID, 8080, twitch)
-
-    # unsubscribe from all old events that might still be there
-    # this will ensure we have a clean slate
-    await event_sub.unsubscribe_all()
-    # start the eventsub client
-    event_sub.start()
-    # subscribing to the desired eventsub hook for our user
-    # the given function (in this example on_follow) will be called every time this event is triggered
-    # the broadcaster is a moderator in their own channel by default so specifying both as the same works in this example
-    # await event_sub.listen_channel_follow_v2(user.id, user.id, on_follow)
-    print('start')
-    await event_sub.listen_stream_online(user.id, on_follow)
-    print('sub')
-
-    # eventsub will run in its own process
-    # so lets just wait for user input before shutting it all down again
-    try:
-        input('press Enter to shut down...')
-    finally:
-        # stopping both eventsub as well as gracefully closing the connection to the API
-        await event_sub.stop()
-        await twitch.close()
-    print('done')
+user_list = {}
 
 
-# lets run our example
-asyncio.run(eventsub_example())
+def get_user(username):
+    params = {
+        "login": username
+    }
+    headers = {
+        "Client-ID": TWITCH_CLIENT_ID,
+        "Authorization": "Bearer {}".format(ACCESS_TOKEN)
+    }
+
+    response = requests.get("https://api.twitch.tv/helix/users", params=params, headers=headers)
+    for entry in response.json()["data"]:
+        user_list[entry["login"]] = entry
+    return {entry["login"]: entry["id"] for entry in response.json()["data"]}
+
+
+def get_stream(users):
+    params = {
+        "user_id": users.values()
+    }
+    headers = {
+        "Client-ID": TWITCH_CLIENT_ID,
+        "Authorization": "Bearer {}".format(ACCESS_TOKEN)
+    }
+
+    response = requests.get("https://api.twitch.tv/helix/streams", params=params, headers=headers)
+    return {entry["user_login"]: entry for entry in response.json()["data"]}
+
+
+online_users = {}
+
+
+def get_notification():
+    users = get_user(TARGET_USERNAME)
+    stream = get_stream(users)
+
+    notification = []
+    for username in TARGET_USERNAME:
+        if username not in online_users and username in stream:
+            stream[username]["info"] = user_list[username]
+            notification.append(stream[username])
+            online_users[username] = True
+        elif username in online_users and username not in stream:
+            online_users.pop(username)
+
+    return notification
+
 
 # discord
-# TOKEN = os.getenv("DISCORD_TOKEN")
-# DISCORD_CHANNEL_ID = os.getenv("DISCORD_CHANNEL_ID")
-#
-# intents = discord.Intents.default()
-# intents.message_content = True
-#
-# client = discord.Client(intents=intents)
-# app = web.Application()
-#
-#
-# @client.event
-# async def on_ready():
-#     print("Logged in as {0.user}".format(client))
-#
-#
-# client.run(TOKEN)
+@ bot.command()
+async def ping(ctx):
+    await ctx.send("pong")
+
+
+@loop(seconds=30)
+async def notification():
+    channel = bot.get_channel(DISCORD_CHANNEL_ID)
+    if not channel:
+        return
+
+    notification = get_notification()
+    for message in notification:
+        # embed
+        embed = discord.Embed(title="{}".format(message["title"]),
+                              url="https://www.twitch.tv/{}".format(message["user_login"]),
+                              description="{}".format(message["info"]["description"]),
+                              color=discord.Color.dark_purple())
+        embed.set_author(name="{}".format(message["user_name"]),
+                         url="https://www.twitch.tv/{}".format(message["user_login"]),
+                         icon_url="{}".format(message["info"]["profile_image_url"]))
+        embed.set_thumbnail(url="{}".format(message["info"]["profile_image_url"]))
+        embed.add_field(name="Game", value="{}".format(message["game_name"]), inline=True)
+        embed.add_field(name="Viewers", value="{}".format(message["viewer_count"]), inline=True)
+        embed.set_image(url="{}".format(message["thumbnail_url"].format(width=1920, height=1080)))
+        embed.set_footer(text="Started streaming：{}" .format(message["started_at"]))
+
+        await channel.send("**{}** 開台啦".format(message['user_name']), embed=embed)
+
+
+@ bot.event
+async def on_ready():
+    notification.start()
+    print("Logged in as {0.user}".format(bot))
+
+
+bot.run(TOKEN)
